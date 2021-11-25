fa9de37f424926bfdc71a3d3a92560274be0f38f
bhargavsai2005 <94927204+bhargavsai2005@users.noreply.github.com>
2021-11-25 22:52:45 +0530

    Add files via upload

---
+++ structure employee in c programe.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+struct employee{
+	char name[20];
+	int age;
+	float salary;
+	int (phonenumber);
+};
+		void main() {
+		struct employee	emp ={"ajay",24,10000,8345700710};
+		struct employee emp ={"ravi",26,15000,9573332250};
+		printf("employee name :%s\n",emp.name);
+		printf("ID = %d\n",emp.id);
+	}
+		
